4c56cf3df7e656c85f6f842e6d1bf95cfcec95fa
Carlos Fortin <cafortinl@hotmail.com>
2020-01-31 13:44:09 -0600

    Segundo commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-int MCD(int num1, int num2){
+using namespace std;
+
+int MCD(int num1, int num2, int div){
 	if(num2 == 0)
 		return num1;
 	else{
-		
+		return MCD(num2,(num1%num2)*10,1);
 	}
 }
 
@@ -23,23 +25,23 @@ void ejercicios(int opcion){
 			int numero1, numero2;
 			cout << "Ingrese el primer numero: ";
 			cin >> numero1;
-			while(numero1 < 0){
+			while(numero1 <= 0){
 				cout << "El numero no puede ser negativo. ";
 				cout << "Ingrese otro numero: ";
 				cin >> numero1;
 			}
 			cout << "Ingrese el segundo numero: ";
 			cin >> numero2;
-			while(numero2 < 0){
+			while(numero2 <= 0){
                                 cout << "El numero no puede ser negativo. ";
                                 cout << "Ingrese otro numero: ";
                                 cin >> numero2;
                         }
 			
 			if(numero1 > numero2)
-				cout << "mcd(" << numero1 << ", " << numero2 << MCD(numero1,numero2) << endl;
+				cout << "mcd(" << numero1 << ", " << numero2 << MCD(numero1,numero2, 1) << endl;
 			else
-				cout << "mcd(" << numero2 << ", " << numero1 << MCD(numero2,numero1) << endl;
+				cout << "mcd(" << numero2 << ", " << numero1 << MCD(numero2,numero1, 1) << endl;
 
 			break;
 		case 2:
